211e14515682562444f2e63211d180809e3ee7db
Blubaeri <evanlin2912@gmail.com>
2022-07-30 13:10:25 -0400

    Add buttons for pagination

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+/*
 const getPokemon = async () => {
   let arr = [];
   for (let i = 1; i <= 24 ; i++) {
@@ -11,101 +11,123 @@ const getPokemon = async () => {
     }    
   return arr;
 }
-
-
-var currentPage = 1;
-var recordsPerPage = 2;
-
-const prevPage = () => {
-  if (currentPage > 1) {
-    currentPage--;
-    changePage(currentPage);
-  }
+*/
+// Create an array for the pokemon
+const getPokemon = async (currentPage) => {
+  let arr = [];
+  for (let i = 1*currentPage; i <= 24*currentPage; i++) {
+    await axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`).
+      then(
+        res => {
+          let pokemon = res.data;
+          arr.push({...pokemon});
+        });
+    }    
+  return arr;
 }
 
-const nextPage = () => {
-  if (currentPage < numPages()) {
-    currentPage++;
-    changePage(currentPage);
-  }
+// Render the display
+const renderDisplay = async (currentPage) => {
+  getPokemon(currentPage).then(pokeArr => {
+    // Create row
+    var row = document.createElement('div');
+    row.setAttribute('class', 'row row-cols-1 row-cols-sm-3 row-cols-md-6 g-0 pokemon-container')
+    pokeArr.forEach((pokemon) => {
+      // Create card and sprite
+      var card = document.createElement('div');
+      var sprite = document.createElement('img');
+
+      // Capitalize first letter
+      const pokeNameUpper = pokemon.name[0].toUpperCase() + pokemon.name.substring(1);
+
+      // Set values of card and sprite
+      sprite.setAttribute('src', pokemon.sprites.front_default);
+      sprite.setAttribute('class', 'card-img-top');
+      card.appendChild(sprite)
+
+      // Set class and style of the card
+      card.setAttribute('class', 'card')
+      card.setAttribute('style', 'width: 15rem;')
+
+      // Card body
+      var cardBody = document.createElement('div');
+      cardBody.setAttribute('class', 'card-body');
+
+      // Card title
+      var cardTitle = document.createElement('h5');
+      cardTitle.setAttribute('class', 'card-title')
+      cardTitle.appendChild(document.createTextNode(pokemon.id + ') ' + pokeNameUpper))
+
+      // Append to card
+      cardBody.appendChild(cardTitle)
+      card.appendChild(cardBody);
+
+      // Append them to the col
+      var col = document.createElement('div');
+      col.setAttribute('class', 'col');
+      col.appendChild(card);
+      row.appendChild(col)
+    })
+    document.getElementById('listingTable').appendChild(row)
+  });
 }
 
-const changePage = (page) => {
-  const btnPrev = document.getElementById('btn-prev');
-  const btnNext = document.getElementById('btn-next');
-  var listing_table = document.getElementById("listingTable");
-  var page_span = document.getElementById("page");
 
-  // Validate page
-  if (page < 1) page = 1;
-  if (page > numPages()) page = numPages();
+var current_page = 1;
+var records_per_page = 12;
 
-  listing_table.innerHTML = "";
+var objJson = getPokemon(current_page);
 
-  for (var i = (page-1) * records_per_page; i < (page * records_per_page); i++) {
-    listing_table.innerHTML += objJson[i].adName + "<br>";
-  }
-  page_span.innerHTML = page;
+function prevPage()
+{
+    if (current_page > 1) {
+        current_page--;
+        changePage(current_page);
+    }
+}
 
-  if (page == 1) {
-    btn_prev.style.visibility = "hidden";
-  } else {
-    btn_prev.style.visibility = "visible";
-  }
+function nextPage()
+{
+    if (current_page < numPages()) {
+        current_page++;
+        changePage(current_page);
+    }
+}
 
-  if (page == numPages()) {
-    btn_next.style.visibility = "hidden";
-  } else {
-    btn_next.style.visibility = "visible";
-  }
+function changePage(page)
+{
+    var btn_next = document.getElementById("btn_next");
+    var btn_prev = document.getElementById("btn_prev");
+    var listing_table = document.getElementById("listingTable");
+    var page_span = document.getElementById("page");
+
+    // Validate page
+    if (page < 1) page = 1;
+    if (page > numPages()) page = numPages();
+
+    listing_table.innerHTML = "";
+
+    renderDisplay(current_page);
+    page_span.innerHTML = page;
+
+    if (page == 1) {
+        btn_prev.style.visibility = "hidden";
+    } else {
+        btn_prev.style.visibility = "visible";
+    }
+
+    if (page == numPages()) {
+        btn_next.style.visibility = "hidden";
+    } else {
+        btn_next.style.visibility = "visible";
+    }
 }
 
 function numPages()
 {
-  return Math.ceil(objJson.length / recordsPerPage);
+    return Math.ceil(objJson.length / records_per_page);
 }
 
-/*
 window.onload = function() {
-  changePage(1);
+    changePage(1);
 };
-*/
-
-getPokemon().then(pokeArr => {
-  pokeArr.forEach((pokemon) => {
-    // Create card and sprite
-    var card = document.createElement('div');
-    var sprite = document.createElement('img');
-
-    // Capitalize first letter
-    const pokeNameUpper = pokemon.name[0].toUpperCase() + pokemon.name.substring(1);
-
-    // Set values of card and sprite
-    sprite.setAttribute('src', pokemon.sprites.front_default);
-    sprite.setAttribute('class', 'card-img-top');
-    card.appendChild(sprite)
-
-    // Set class and style of the card
-    card.setAttribute('class', 'card')
-    card.setAttribute('style', 'width: 15rem;')
-
-    // Card body
-    var cardBody = document.createElement('div');
-    cardBody.setAttribute('class', 'card-body');
-
-    // Card title
-    var cardTitle = document.createElement('h5');
-    cardTitle.setAttribute('class', 'card-title')
-    cardTitle.appendChild(document.createTextNode(pokemon.id + ') ' + pokeNameUpper))
-
-    // Append to card
-    cardBody.appendChild(cardTitle)
-    card.appendChild(cardBody);
-
-    // Append them to the col
-    var col = document.createElement('div');
-    col.setAttribute('class', 'col');
-    col.appendChild(card);
-    document.getElementById('pokemon-container').appendChild(col)
-  })
-});
